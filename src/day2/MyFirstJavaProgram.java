a7bc3b52b392d6feaef81a0ed79cac7e57069eb4
Kai Salinas <league@WTS14.attlocal.net>
2018-01-03 14:54:25 -0800

    Day 2

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -1,15 +1,32 @@
 package day2;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot r2d2 = new Robot();
+		r2d2.setSpeed(100);
+		r2d2.sparkle();
+		r2d2.move(100);
+		r2d2.miniaturize();
+		r2d2.move(-100);
+		r2d2.setRandomPenColor();
+		r2d2.penDown();
+		r2d2.move(200);
+		r2d2.turn(90);
+		r2d2.setRandomPenColor();
+		r2d2.move(200);
+		r2d2.turn(90);
+		r2d2.setRandomPenColor();
+		r2d2.expand();
+		r2d2.move(200);
+		r2d2.expand();
+		r2d2.turn(90);
+		r2d2.setRandomPenColor();
+		r2d2.move(200);
+
 	}
 }
--- src/day2/SmartShapes.java
+++ src/day2/SmartShapes.java
@@ -3,27 +3,24 @@ package day2;
 import org.jointheleague.graphical.robot.Robot;
 
 public class SmartShapes {
-	
-    public static void main(String[] args) throws Exception {
-        // 1. Make a new Robot
 
+	public static void main(String[] args) throws Exception {
+		// 1. Make a new Robot
+		Robot r2d2 = new Robot();
+		r2d2.moveTo(200, 400);
+		// 3. Put the robot's pen down
+		r2d2.penDown();
+		// 6. Make the robot move as fast as possible
+		r2d2.setSpeed(100);
+		// 5. Do everything below here 4 times
+		for (int i = 0; i < 10; i++) {
+			// 2. Move your robot 200 pixels
+			r2d2.move(200);
 
-        // 3. Put the robot's pen down
-
-
-        // 6. Make the robot move as fast as possible
-
-
-        // 5. Do everything below here 4 times
-
-
-        //         2. Move your robot 200 pixels
-
-
-        //         4. Turn the robot 90 degrees to the right
-
-    	
-    		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
-   
-    }
+			// 4. Turn the robot 90 degrees to the right
+			r2d2.turn(360 / 10);
+			// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides),
+			// pentagon (5-sides), decagon (10-sides)).
+		}
+	}
 }
